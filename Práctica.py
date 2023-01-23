371df672d2cc7d38760668e784b7e66b72d11204
Diego Cali <110750625+DiegoCali@users.noreply.github.com>
2023-01-23 13:10:03 -0600

    Translation

--- Práctica.py
+++ Práctica.py
@@ -2,22 +2,22 @@ import tkinter as tk
 from random import randint
 from tkinter import messagebox
 vent=tk.Tk()
-vent.title("El conquista minas 3000")
+vent.title("To My Valentine")
 vent.geometry('300x300')
-def move_btn(evento):
-    evento.widget.place(x=randint(0,250), y=randint(0,250))
+def move_btn(event):
+    event.widget.place(x=randint(0,250), y=randint(0,250))
 
 def pressed():
-    messagebox.showinfo("titulo", "Sabía que aceptarías :3")
+    messagebox.showinfo("title", "I knew you'd say yes <3")
 
-titulo=tk.Label(vent, text='¿Quiéres ser mi novia?')
-titulo.pack(padx=20, pady=20)
+title=tk.Label(vent, text='Would you be my GF?')
+title.pack(padx=20, pady=20)
 
-boton1=tk.Button(vent, text='NO', height='1', width='8')
-boton1.place(x=180, y=170)
-boton1.bind("<Enter>", move_btn)
-boton2=tk.Button(vent, text='SI', height='1', width='8', command=pressed)
-boton2.place(x=50, y=170)
+button1=tk.Button(vent, text='NO', height='1', width='8')
+button1.place(x=180, y=170)
+button1.bind("<Enter>", move_btn)
+button2=tk.Button(vent, text='YES', height='1', width='8', command=pressed)
+button2.place(x=50, y=170)
 
 
 vent.mainloop()
